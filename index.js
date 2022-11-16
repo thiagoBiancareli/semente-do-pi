2720cf8ed3ad3b68d4a7fb5c1b5980284cb11141
thiagoBiancareli <thiagob2mt@gmail.com>
2022-11-16 08:25:10 -0300

    Servidor configurado e criado

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Está configurado!");
+// Importar express
+const express = require("express");
+
+// Criar o servidor
+const servidor = express();
+
+// Definir a public como pasta de arquivos estáticos
+servidor.use(express.static(__dirname + '/public'));
+
+// Definir as rotas
+servidor.get('/produtos', (req, res)=>{
+    return res.sendFile(__dirname + '/views/produtos.html');
+})
+
+// Fazer meu servidor rodar
+servidor.listen(3000);
+
+
